7e34eefefccced0814d9fb42962741b20ea98e93
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-06-30 17:52:39 +0800

    add redis testing.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -3,9 +3,6 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class UserTest extends TestCase
 {
@@ -17,5 +14,24 @@ class UserTest extends TestCase
     public function testExample()
     {
         $this->assertTrue(true);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testRedis()
+    {
+        // Retrieve a piece of data from the session...
+        $value = session('key');
+
+        // Specifying a default value...
+        $value = session('key', 'default');
+        $this->assertEquals('default', session('key', 'default'));
+
+        // Store a piece of data in the session...
+        session(['key' => 'value']);
+        $this->assertEquals('value', session('key', 'default'));
     }
 }
